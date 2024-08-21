e8974659e1946b3a6ea3f3b37ae7c6d0a5568883
Misha Kav <misha.kav@gmail.com>
2024-08-21 12:35:42 +0300

    chore: Refactor extractOrcaFindings filter to use orcasecurity in comments

--- .cm/plugins/filters/extractOrcaFindings/index.js
+++ .cm/plugins/filters/extractOrcaFindings/index.js
@@ -2,22 +2,21 @@ function getOrcaPropertyRating(lines, lineIdentifierRegex, findingsCellIndex) {
   const matches = lines.filter(x => x.match(lineIdentifierRegex));
   const [firstMatch] = matches;
   const cells = firstMatch.split('|');
-  const [_, high, medium, low, info] =
-    /"High"> ([\d]+).*"Medium"> ([\d]+).*"Low"> ([\d]+).*"Info"> ([\d]+)/
-      .exec(cells[findingsCellIndex])
-      .map(x => parseInt(x));
-  return { high, medium, low, info };
+  const [_, high, medium, low, info] = /"High"> ([\d]+).*"Medium"> ([\d]+).*"Low"> ([\d]+).*"Info"> ([\d]+)/
+    .exec(cells[findingsCellIndex])
+    .map(x => parseInt(x));
+  return {high, medium, low, info};
 }
 
-module.exports = pr => {
+module.exports = (pr) => {
   let orcaObject = {
     infrastructure_as_code: { count: null, rating: '' },
     vulnerabilities: { count: null, rating: '' },
-    secrets: { count: null, rating: '' }
+    secrets: { count: null, rating: '' },
   };
 
   // Orca comments are added as PR review
-  const orcaComment = pr.comments.filter(x => x.content?.includes('orcasecurity'));
+  const orcaComment = pr.comments.filter(x => x.content.includes('orca-security'));
 
   if (orcaComment.length) {
     const orcaCommentArray = orcaComment[0].content.split('\n');
@@ -25,21 +24,21 @@ module.exports = pr => {
     var rating = getOrcaPropertyRating(orcaCommentArray, /Infrastructure as Code/, 3);
     orcaObject.infrastructure_as_code = {
       count: rating.high + rating.medium + rating.low + rating.info,
-      rating
+      rating,
     };
 
     var rating = getOrcaPropertyRating(orcaCommentArray, /Vulnerabilities/, 3);
     orcaObject.vulnerabilities = {
       count: rating.high + rating.medium + rating.low + rating.info,
-      rating
+      rating,
     };
 
     var rating = getOrcaPropertyRating(orcaCommentArray, /Secrets/, 3);
     orcaObject.secrets = {
       count: rating.high + rating.medium + rating.low + rating.info,
-      rating
+      rating,
     };
   }
 
   return JSON.stringify(orcaObject);
-};
+}
