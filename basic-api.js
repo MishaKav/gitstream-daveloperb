e87f4014504275c2f4bd1a51b458ba6b93df7b71
Misha Kav <misha.kav@gmail.com>
2025-05-27 22:01:03 +0300

    feat: add basic API routes for book management

---
+++ basic-api.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const BooksController = require('../controllers/booksController');
+
+const router = express.Router();
+const booksController = new BooksController();
+
+router.post('/books', booksController.createBook.bind(booksController));
+router.get('/books', booksController.getBooks.bind(booksController));
+router.get('/books/:id', booksController.getBookById.bind(booksController));
+router.put('/books/:id', booksController.updateBook.bind(booksController));
+router.delete('/books/:id', booksController.deleteBook.bind(booksController));
+
+module.exports = router;
